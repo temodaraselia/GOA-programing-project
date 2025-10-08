79a5b919ce63aa582a49cb0b4547eb48a4ec9e8e
temodaraselia <temuridaraselia5@gmail.com>
2025-10-08 19:12:49 +0400

    second commit

---
+++ day 3 python/classwork/cw1.py
@@ -0,0 +1,16 @@
+#1) დაწერეთ კოდი სადაც user-ს მოთხოვ სახელს, და შემდგომ ის სახელი გამოიტანეთ მაგ: "Hello, (name)"temo
+
+
+name=input("enter your name:")
+print("hello " + name)
+
+
+
+
+
+
+
+
+
+
+
---
+++ day 3 python/classwork/cw3.py
@@ -0,0 +1,21 @@
+#3)დაწერეთ კოდი სადაც მომხმარებელს მოთხოვ საყვარელ ფერს, რიცხვს, ცხოველს, და შემდეგ გამოიტანეთ (გამოიყენეთ დაახლოებით 3ჯერ ან მეტჯერ)
+
+color=input("enter your favorite color:")
+
+number=int(input("enter your favorite number:"))
+
+animal=input("enter your favorite animal:")
+
+
+print(color)
+print(number)
+print(animal)
+
+
+
+
+
+
+
+
+
