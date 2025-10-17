8c26f04c4157831d3abfa2de190edfff629e5273
temodaraselia <temuridaraselia5@gmail.com>
2025-10-17 14:24:25 +0400

    second commit

---
+++ day 6 python/homework/hw3.py
@@ -0,0 +1,12 @@
+#5) რას გამოიტანს True and False or True or False and False and True or False and False 
+
+print(True and False)
+
+#print(True or False)     # True
+
+#print(False and True)    # False
+
+#print(False and False)   # False
+
+#print(True and False or True or False and False and True or False and False)   #  True
+
