b7babc08301d68393e1caafe041ea65ea2d85e7d
temodaraselia <temuridaraselia5@gmail.com>
2025-11-05 00:47:17 +0400

    second commit

---
+++ day 11 python/classwork/cw3.py
@@ -0,0 +1,11 @@
+#3) ჯერ გამოიტანეთ ეს მთლიანი სია და შემდეგ სიის მენულე, მეორე და მეოთხე ინდექსი
+
+number_list=[1,2,3,4,5]
+print(number_list[0:4],)
+print(number_list[0])
+print(number_list[2] ,number_list[4])
+
+
+
+
+
---
+++ day 11 python/classwork/cw4.py
@@ -0,0 +1,24 @@
+#4) გამოიტანეთ სიის ბოლო ელემენტი (შემთხვევა სადაც არ იცით რამხელაა თქვენი სია, მინიშნება: -1)
+
+cars_company=["BMW","MERCEDES","AUDI","TOYOTA","BUGGATI"]
+print(cars_company[-1],"and",cars_company[-5])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
