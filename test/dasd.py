46d7194f59dda640ef56a2a90494e02c4c6fa528
temodaraselia <temuridaraselia5@gmail.com>
2026-01-07 20:29:59 +0400

    second commit

--- test/dasd.py
+++ test/dasd.py
@@ -7,3 +7,74 @@
 
 
 
+# def find_multiples(n, limit):
+#     total=0
+#     while total!=limit-n:
+#         total=n+n
+#     return n,total,limit  
+ 
+# print(find_multiples(5,25))
+
+
+
+
+# def count_positives_sum_negatives(arr):
+#     if arr==0:
+#         return 0
+#     else:
+#         return sum(arr)>0
+#         return sum(arr)<0
+
+# arr=[5,10,7,5,-5,-7,-5,-10]
+# print(count_positives_sum_negatives(arr))
+
+# def powers_of_two(n):
+#     if n==0:
+#         return 1
+#     elif n>0:
+#         arr=[]
+#         while len(arr)!=int(n):
+#             arr+=n 
+#     return arr
+# print(powers_of_two(2))
+# def say_hello(name):
+#     a=["Hello",name]
+#     w=", ".join(a)
+#     return w
+# print(say_hello("Mr.Beast"))
+
+
+
+
+
+# def between(a,b):
+#     if a<b:
+#         while a!=b:
+#             total=[]
+#             total=a+1
+#     return total
+# print(between(1,5))
+
+
+# def tower_builder(n_floors):
+#     if n_floors>0:
+#         return "*" * n_floors
+# print(tower_builder(5))
+
+
+# def min_max(lst):
+#     if len(lst)>2:
+#         return min(lst),max(lst)
+
+
+
+
+
+# def solution(nums):
+#     return nums.sort()
+ 
+
+# print(solution([1,2,3,6,1,810,8]))
+
+
+
